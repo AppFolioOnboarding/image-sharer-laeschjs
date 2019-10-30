13e174eecd99b83830a776091a29e3b01c661901
Joshua Laesch <joshua.laesch@appfolio.com>
2019-10-30 13:36:26 -0700

    jl - Added create action for an image

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,4 +2,21 @@ class ImagesController < ApplicationController
   def new
     @image = Image.new
   end
+
+  def create
+    @image = Image.new(create_params)
+
+    if @image.save
+      flash[:notice] = 'success'
+      redirect_to @image
+    else
+      render :new, status: :bad_request
+    end
+  end
+
+  private
+
+  def create_params
+    params.require(:image).permit(:url)
+  end
 end
--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -8,4 +8,21 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
     assert_select 'label', 'Enter the Image URL:'
     assert_select 'form input[type=submit][value="Save Image"]'
   end
+
+  test 'should not create' do
+    assert_no_difference 'Image.count' do
+      post images_path, params: { image: { url: 'bad url' } }
+    end
+    assert_equal 400, status
+  end
+
+  test 'should create' do
+    assert_difference 'Image.count' do
+      post images_path, params: { image:
+                                    {
+                                      url: 'https://partycity6.scene7.com/is/image/PartyCity/_pdp_sq_?$_1000x1000_$&$product=PartyCity/P590860'
+                                    } }
+    end
+    assert_redirected_to image_path(Image.last)
+  end
 end
