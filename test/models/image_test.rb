33e40802814523e675132e2b8d5df3de4da0f019
Joshua Laesch <joshua.laesch@appfolio.com>
2019-11-01 14:54:36 -0700

    jl - add tags to Image model

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class ImageTest < ActiveSupport::TestCase
+  setup do
+    @image = Image.new(url: 'https://partycity6.scene7.com/is/image/PartyCity/_pdp_sq_?$_1000x1000_$&$product=PartyCity/P590860')
+    @tags = %w[test test2]
+  end
+
   test 'nil url' do
     i = Image.new(url: nil)
     refute i.valid?
@@ -14,8 +19,25 @@ class ImageTest < ActiveSupport::TestCase
   end
 
   test 'valid url' do
-    i = Image.new(url: 'https://partycity6.scene7.com/is/image/PartyCity/_pdp_sq_?$_1000x1000_$&$product=PartyCity/P590860')
-    assert i.valid?
-    assert_empty i.errors.messages
+    assert @image.valid?
+    assert_empty @image.errors.messages
+  end
+
+  test 'image contains tags as array' do
+    @image.tag_list = @tags
+    @image.save
+    @image.reload
+    @image.tags.each_with_index do |tag, index|
+      assert_equal tag.name, @tags[index]
+    end
+  end
+
+  test 'image contains tags as string' do
+    @image.tag_list = 'test, test2'
+    @image.save
+    @image.reload
+    @image.tags.each_with_index do |tag, index|
+      assert_equal tag.name, @tags[index]
+    end
   end
 end
