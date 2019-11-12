a8911ab874940127a930a8ca585c96d3f275c44a
Joshua Laesch <joshua.laesch@appfolio.com>
2019-11-12 14:26:16 -0800

    jl - add footer view

--- app/javascript/image_sharing/components/Footer.js
+++ app/javascript/image_sharing/components/Footer.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Col, Row } from 'reactstrap';
 
 class Footer extends Component {
   /* Implement your Footer component here */
+  static propTypes = {
+    content: PropTypes.string.isRequired
+  };
+
+  render() {
+    return (
+      <div className="fixed-bottom">
+        <Row>
+          <Col lg={{ size: 4, offset: 4 }}>
+            <p id='js-footer-text' className='text-center'>
+              {this.props.content}
+            </p>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default Footer;
