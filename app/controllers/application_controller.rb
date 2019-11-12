47ef7bca7778a25eac55dbe8e5516773c8f33a6d
Joshua Laesch <joshua.laesch@appfolio.com>
2019-11-12 15:01:18 -0800

    jl - add link from rails part to react part

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def home
-    redirect_to controller: 'feedbacks', action: :new
-  end
+  def home; end
 end
