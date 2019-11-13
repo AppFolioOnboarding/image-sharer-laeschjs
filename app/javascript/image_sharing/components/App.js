f2ba69902f120653d28592c3681597d31f1ec1ce
Joshua Laesch <joshua.laesch@appfolio.com>
2019-11-13 14:45:58 -0800

    jl - added a feedback form to the app

--- app/javascript/image_sharing/components/App.js
+++ app/javascript/image_sharing/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { inject } from 'mobx-react';
 import Header from './Header';
 import Footer from './Footer';
+import FeedbackForm from './FeedbackForm';
 
 class App extends Component {
   /* Add Prop Types check*/
@@ -10,6 +11,7 @@ class App extends Component {
       <div>
         <Header title='Tell us what you think' />
         {/* Put your components here: Flash Message, Form, Footer */}
+        <FeedbackForm store={this.props.stores.feedbackStore} />
         <Footer content='Copyright &copy;: Appfolio Inc. Onboarding' />
       </div>
     );
