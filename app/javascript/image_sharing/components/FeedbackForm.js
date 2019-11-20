27d227a051c029e21cdcda2c32fbfa15560c9ab7
Joshua Laesch <joshua.laesch@appfolio.com>
2019-11-20 11:49:39 -0800

    jl - update feedback form to be able to submit

--- app/javascript/image_sharing/components/FeedbackForm.js
+++ app/javascript/image_sharing/components/FeedbackForm.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import { observer } from 'mobx-react';
+import { observer, inject } from 'mobx-react';
 import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import { PostFeedbackService } from '../services/PostFeedbackService';
 
+@inject('feedbackStore')
+@inject('uiStore')
 @observer
 class FeedbackForm extends Component {
   render() {
-    const store = this.props.store;
+    const store = this.props.feedbackStore;
     return (
       <Form>
         <FormGroup>
@@ -16,9 +19,15 @@ class FeedbackForm extends Component {
           <Label for="comment">Comment</Label>
           <Input type="text" id='comment' value={store.feedbackDetails.comment} onChange={store.setAttr('comment')} />
         </FormGroup>
-        <Button color='primary'>Submit</Button>
+        <Button onClick={this.submit} color='primary'>Submit</Button>
       </Form>
     );
+  }
+
+  submit = () => {
+    PostFeedbackService.submit(this.props.feedbackStore, (response) => {
+      this.props.uiStore.setSubmit(response.result);
+    });
   }
 }
 
